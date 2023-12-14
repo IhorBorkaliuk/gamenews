c1e8d9e8dd73ce521d41090affa08f11b5564dbc
Ihor Borkaliuk <happyviolence1990@gmail.com>
2023-12-14 13:57:47 +0100

    Update PostCard.jsx

--- src/components/PostCard/PostCard.jsx
+++ src/components/PostCard/PostCard.jsx
@@ -1,30 +1,29 @@
-import React from 'react'
+import React from 'react';
 import Card from 'react-bootstrap/Card';
-import { useNavigate} from 'react-router-dom';
+import { useNavigate } from 'react-router-dom';
 import { StyledNavLink } from './PostCardStyled';
 
-
 export const PostCard = ({ el }) => {
-   const navigate = useNavigate();
+  const navigate = useNavigate();
 
-   const handleClick = () => {
-     navigate(`/${el.id}`, { state: { el } });
-   };
-return (
-  <Card className="h-100" onClick={handleClick}>
-    <Card.Img variant="top" src={el.main_image} alt={el.short_description} />
-    <StyledNavLink
-      to={`/${el.id}`}
-      rel="noopener noreferrer"
-      className="stretched-link"
-    >
-      <Card.Body>
-        <Card.Title>{el.title}</Card.Title>
-        <Card.Text>{el.short_description}</Card.Text>
-      </Card.Body>
-    </StyledNavLink>
-  </Card>
-);
-}
+  const handleClick = () => {
+    navigate(`/${el.id}`, { state: { el } });
+  };
+  return (
+    <Card className="h-100" onClick={handleClick}>
+      <Card.Img variant="top" src={el.main_image} alt={el.short_description} />
+      <StyledNavLink
+        to={'/:id'}
+        rel="noopener noreferrer"
+        className="stretched-link"
+      >
+        <Card.Body>
+          <Card.Title>{el.title}</Card.Title>
+          <Card.Text>{el.short_description}</Card.Text>
+        </Card.Body>
+      </StyledNavLink>
+    </Card>
+  );
+};
 
-export default PostCard
+export default PostCard;
