2f06d2ceb9607da5df8bed8c7bc5738a29d9f9bd
Ihor Borkaliuk <happyviolence1990@gmail.com>
2024-08-04 16:55:00 +0200

    Update Reviews.jsx

--- src/components/Reviews/Reviews.jsx
+++ src/components/Reviews/Reviews.jsx
@@ -1,6 +1,7 @@
-import React from 'react'
+import React, { useState } from 'react'
 
 export const Reviews = () => {
+  const [reviews, setReviews] = useState([])
   return (
     <div>Reviews</div>
   )
